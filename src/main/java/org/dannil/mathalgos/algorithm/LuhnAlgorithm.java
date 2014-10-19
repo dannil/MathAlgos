7e967875ea4c518b74fb93c1faedcf4c932ecc33
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-19 14:00:52 +0200

    Added optimized way to calculate Luhn algorithm

--- src/main/java/org/dannil/mathalgos/algorithm/LuhnAlgorithm.java
+++ src/main/java/org/dannil/mathalgos/algorithm/LuhnAlgorithm.java
@@ -15,6 +15,7 @@ public class LuhnAlgorithm {
 	 *            - The number to calculate.
 	 * @return An integer which has been calculated by the algorithm.
 	 */
+
 	public static final int calculate(int t) {
 		List<Integer> digits = new LinkedList<Integer>();
 		List<Integer> result = new LinkedList<Integer>();
@@ -52,6 +53,44 @@ public class LuhnAlgorithm {
 		int sumToReturn = 0;
 		for (int z = 0; z < result.size(); z++) {
 			sumToReturn += result.get(z);
+		}
+		return sumToReturn;
+	}
+
+	public static final int calculate_new(int t) {
+		List<Integer> digits = new LinkedList<Integer>();
+		List<Integer> result = new LinkedList<Integer>();
+
+		// Step 1: Split the number into it's digits and store them in a list
+		digits = NumberHelper.splitIntoDigits(t);
+
+		// Step 2: For every digit, multiply the digits which has even
+		// positions with 2 and the digits which has uneven positions with 1
+		// Save this new sequence of numbers into a separate list
+		for (int x = 0; x < digits.size(); x++) {
+			int res = 0;
+			if (x % 2 == 0) {
+				res = 2 * digits.get(x);
+			} else {
+				res = digits.get(x);
+			}
+
+			// Step 3: Similar to step 1; if we detect that the number is equal
+			// to or greater than 10, we split that number into it's digits
+			// and remove the detected number from the list. We then insert our
+			// new numbers we got after the split into the old numbers position
+			if (res >= 10) {
+				List<Integer> temp = NumberHelper.splitIntoDigits(res);
+				result.addAll(temp);
+			} else {
+				result.add(res);
+			}
+		}
+
+		// Step 4: Sum up all the numbers in the list and return the result
+		int sumToReturn = 0;
+		for (int y = 0; y < result.size(); y++) {
+			sumToReturn += result.get(y);
 		}
 		return sumToReturn;
 	}
