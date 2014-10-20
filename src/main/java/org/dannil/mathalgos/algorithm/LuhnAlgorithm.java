5f96fc014a801e15e8c32d2faf7c70744b96dbde
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-20 21:13:45 +0200

    Deleted old inefficient method

--- src/main/java/org/dannil/mathalgos/algorithm/LuhnAlgorithm.java
+++ src/main/java/org/dannil/mathalgos/algorithm/LuhnAlgorithm.java
@@ -15,49 +15,7 @@ public class LuhnAlgorithm {
 	 *            - The number to calculate.
 	 * @return An integer which has been calculated by the algorithm.
 	 */
-
 	public static final int calculate(int t) {
-		List<Integer> digits = new LinkedList<Integer>();
-		List<Integer> result = new LinkedList<Integer>();
-
-		// Step 1: Split the number into it's digits and store them in a list
-		digits = NumberHelper.splitIntoDigits(t);
-
-		// Step 2: For every digit, multiply the digits which has even
-		// positions with 2 and the digits which has uneven positions with 1
-		// Save this new sequence of numbers into a separate list
-		for (int x = 0; x < digits.size(); x++) {
-			int res = 0;
-			if (x % 2 == 0) {
-				res = 2 * digits.get(x);
-			} else {
-				res = digits.get(x);
-			}
-			result.add(res);
-		}
-
-		// Step 3: Similar to step 1; if we detect a number that's equal to
-		// or greater than 10, we split that number into it's digits and remove
-		// the detected number from the list. We then insert our new numbers
-		// we got after the split into the old numbers position
-		for (int y = 0; y < result.size(); y++) {
-			int numberFromResultList = result.get(y);
-			if (numberFromResultList >= 10) {
-				List<Integer> temp = NumberHelper.splitIntoDigits(numberFromResultList);
-				result.remove(y);
-				result.addAll(y, temp);
-			}
-		}
-
-		// Step 4: Sum up all the numbers in the list and return the result
-		int sumToReturn = 0;
-		for (int z = 0; z < result.size(); z++) {
-			sumToReturn += result.get(z);
-		}
-		return sumToReturn;
-	}
-
-	public static final int calculate_new(int t) {
 		List<Integer> digits = new LinkedList<Integer>();
 		List<Integer> result = new LinkedList<Integer>();
 
