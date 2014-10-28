399ef41f848a868144569cf73780a105f53a535c
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-28 21:50:53 +0100

    Added helper methods

--- src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
+++ src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
@@ -1,6 +1,7 @@
 package org.dannil.mathalgos.algorithm.helper;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -38,5 +39,43 @@ public class NumberHelper {
 		BigDecimal x = new BigDecimal(Math.sqrt(value.doubleValue()));
 		return x.add(new BigDecimal(value.subtract(x.multiply(x)).doubleValue() / (x.doubleValue() * 2.0)));
 	}
+
+	public static BigInteger bigIntSqRootFloor(BigInteger x) throws IllegalArgumentException {
+		if (x.compareTo(BigInteger.ZERO) < 0) {
+			throw new IllegalArgumentException("Negative argument.");
+		}
+		// square roots of 0 and 1 are trivial and
+		// y == 0 will cause a divide-by-zero exception
+		if (x == BigInteger.ZERO || x == BigInteger.ONE) {
+			return x;
+		} // end if
+		BigInteger two = BigInteger.valueOf(2L);
+		BigInteger y;
+		// starting with y = x / 2 avoids magnitude issues with x squared
+		for (y = x.divide(two); y.compareTo(x.divide(y)) > 0; y = ((x.divide(y)).add(y)).divide(two))
+			;
+		return y;
+	} // end bigIntSqRootFloor
+
+	public static BigInteger bigIntSqRootCeil(BigInteger x) throws IllegalArgumentException {
+		if (x.compareTo(BigInteger.ZERO) < 0) {
+			throw new IllegalArgumentException("Negative argument.");
+		}
+		// square roots of 0 and 1 are trivial and
+		// y == 0 will cause a divide-by-zero exception
+		if (x == BigInteger.ZERO || x == BigInteger.ONE) {
+			return x;
+		} // end if
+		BigInteger two = BigInteger.valueOf(2L);
+		BigInteger y;
+		// starting with y = x / 2 avoids magnitude issues with x squared
+		for (y = x.divide(two); y.compareTo(x.divide(y)) > 0; y = ((x.divide(y)).add(y)).divide(two))
+			;
+		if (x.compareTo(y.multiply(y)) == 0) {
+			return y;
+		} else {
+			return y.add(BigInteger.ONE);
+		}
+	} // end bigIntSqRootCeil
 
 }
