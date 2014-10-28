5ee310ec4a488b0b1896f62f34dab4de6df84bea
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-28 21:50:44 +0100

    Fixed bugs

--- src/main/java/org/dannil/mathalgos/algorithm/PiChudnovsky.java
+++ src/main/java/org/dannil/mathalgos/algorithm/PiChudnovsky.java
@@ -1,77 +1,77 @@
 package org.dannil.mathalgos.algorithm;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.math.RoundingMode;
 import java.util.LinkedList;
 
 import org.dannil.mathalgos.algorithm.helper.NumberHelper;
 
+/**
+ * <p>Class that implements the Chudnovsky brothers solution of computing pi
+ * to a specified amount of decimal places</p>
+ * 
+ * </p>Credits to Nick Craig-Wood for showing a Python implementation which I used
+ * as a basis for this class (see http://www.craig-wood.com/nick/articles/pi-chudnovsky/)</p>
+ * 
+ * @author Daniel Nilsson
+ *
+ */
 public class PiChudnovsky {
 
 	/* Magic numbers used for making the calculation work */
-	private static final BigInteger TENTHOUSHAND = new BigInteger("10005");
-	private static final BigInteger C = new BigInteger("640320");
-	private static final BigInteger FOURHUNDREDTHOUSAND = new BigInteger("426880");
-	private static final BigInteger THIRTEENMILLION = new BigInteger("13591409");
-	private static final BigInteger FIVEHUNDREDFOURTYFIVEMILLION = new BigInteger("545140134");
-
 	private static final BigInteger MINUS_ONE = new BigInteger("-1");
 	private static final BigInteger TWO = new BigInteger("2");
 	private static final BigInteger FIVE = new BigInteger("5");
 	private static final BigInteger SIX = new BigInteger("6");
 	private static final BigInteger TWENTYFOUR = new BigInteger("24");
-
+	private static final BigInteger TENTHOUSHAND = new BigInteger("10005");
+	private static final BigInteger C = new BigInteger("640320");
+	private static final BigInteger FOURHUNDREDTHOUSAND = new BigInteger("426880");
+	private static final BigInteger THIRTEENMILLION = new BigInteger("13591409");
+	private static final BigInteger FIVEHUNDREDFOURTYFIVEMILLION = new BigInteger("545140134");
 	private static final BigInteger C3_OVER_24 = C.pow(3).divide(TWENTYFOUR);
 
-	private static BigDecimal P;
-	private static BigDecimal Q;
-	private static BigDecimal T;
-
-	private static BigDecimal Pab;
-	private static BigDecimal Qab;
-	private static BigDecimal Tab;
-
-	public static BigDecimal debug() {
-		System.out.println("C3_OVER_24: " + C3_OVER_24);
-		return computePi(new BigInteger("50"));
+	/**
+	 * Computes Pi to the specified amount of decimals and returns it
+	 * as a string with correct delimite
+	 * 
+	 * @param decimals The amount of decimals to calculate
+	 * @return A String representation of a BigInteger containing truncated pi
+	 */
+	public static String computePiStringPresentation(BigInteger decimals) {
+		String piString = computePi(decimals).toString();
+		return new StringBuffer(piString).insert(1, ".").toString();
 	}
 
 	/**
 	 * Computes Pi to the specified amount of decimals
 	 * 
 	 * @param decimals The amount of decimals to calculate
-	 * @return A BigDecimal containing truncated Pi
+	 * @return A BigInteger containing truncated pi
 	 */
-	public static BigDecimal computePi(BigInteger decimals) {
+	public static BigInteger computePi(BigInteger decimals) {
 		final double DIGITS_PER_TERM = Math.log10(C3_OVER_24.doubleValue() / 6 / 2 / 6);
 		System.out.println("DIGITS_PER_TERM: " + DIGITS_PER_TERM);
 
 		final BigInteger N = BigInteger.valueOf((long) (decimals.doubleValue() / DIGITS_PER_TERM + 1));
 		System.out.println("N: " + N);
 
-		LinkedList<BigDecimal> list = null;
-		try {
-			list = calculateTerms(BigInteger.ZERO, N);
-			list.toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		LinkedList<BigInteger> list = calculateTerms(BigInteger.ZERO, N);
 
-		P = list.get(0);
-		Q = list.get(1);
-		T = list.get(2);
-
-		BigDecimal ONE_SQUARED = BigDecimal.TEN.pow(2 * decimals.intValue());
-		System.out.println("One squared: " + ONE_SQUARED);
-		BigDecimal SQRT_C = NumberHelper.sqrt(new BigDecimal(TENTHOUSHAND).multiply(ONE_SQUARED));
-		System.out.println("SQRT_C: " + SQRT_C);
+		BigInteger P = list.get(0);
+		BigInteger Q = list.get(1);
+		BigInteger T = list.get(2);
 
 		System.out.println("Q: " + Q);
-		BigDecimal FINAL_DECIMAL = Q.multiply(new BigDecimal(FOURHUNDREDTHOUSAND)).multiply(SQRT_C).divide(T, RoundingMode.FLOOR);
-		FINAL_DECIMAL = FINAL_DECIMAL.setScale(0, RoundingMode.DOWN);
+
+		BigInteger ONE_SQUARED = BigInteger.TEN.pow(2 * decimals.intValue());
+		System.out.println("One squared: " + ONE_SQUARED);
+
+		BigInteger SQRT_C = NumberHelper.bigIntSqRootCeil(TENTHOUSHAND.multiply(ONE_SQUARED));
+		System.out.println("SQRT_C: " + SQRT_C);
+
+		BigInteger FINAL_DECIMAL = Q.multiply(FOURHUNDREDTHOUSAND).multiply(SQRT_C).divide(T);
 		System.out.println(FINAL_DECIMAL);
-		return null;
+		return FINAL_DECIMAL;
 	}
 
 	/**
@@ -90,30 +90,33 @@ public class PiChudnovsky {
 	 * @return P(a,b), Q(a,b) and T(a,b)
 	 * @throws Exception
 	 */
-	private static LinkedList<BigDecimal> calculateTerms(BigInteger a, BigInteger b) throws Exception {
+	private static LinkedList<BigInteger> calculateTerms(BigInteger a, BigInteger b) {
+		BigInteger Pab;
+		BigInteger Qab;
+		BigInteger Tab;
 		System.out.println("B - A: " + b.subtract(a));
 		if (b.subtract(a).equals(BigInteger.ONE)) {
 			System.out.println("Equals 1");
 			// Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
 			if (a.equals(BigInteger.ZERO)) {
 				System.out.println("Equals 0");
-				BigDecimal tmp1 = BigDecimal.valueOf(1);
+				BigInteger tmp1 = new BigInteger("1");
 				Pab = tmp1;
 				Qab = tmp1;
 				System.out.println("Pab: " + Pab);
 			} else {
-				BigDecimal tmp1 = new BigDecimal(a.multiply(SIX).subtract(FIVE));
-				BigDecimal tmp2 = new BigDecimal(a.multiply(TWO).subtract(BigInteger.ONE));
-				BigDecimal tmp3 = new BigDecimal(a.multiply(SIX).subtract(BigInteger.ONE));
+				BigInteger tmp1 = a.multiply(SIX).subtract(FIVE);
+				BigInteger tmp2 = a.multiply(TWO).subtract(BigInteger.ONE);
+				BigInteger tmp3 = a.multiply(SIX).subtract(BigInteger.ONE);
 				Pab = tmp1.multiply(tmp2).multiply(tmp3);
 				System.out.println("Pab in else: " + Pab);
 
-				Qab = new BigDecimal(C3_OVER_24.multiply(a).multiply(a).multiply(a));
+				Qab = C3_OVER_24.multiply(a).multiply(a).multiply(a);
 				System.out.println("Qab in else: " + Qab);
 			}
 
 			// a(a) * p(a)
-			Tab = Pab.multiply(new BigDecimal(a.multiply(FIVEHUNDREDFOURTYFIVEMILLION).add(THIRTEENMILLION)));
+			Tab = Pab.multiply(a.multiply(FIVEHUNDREDFOURTYFIVEMILLION).add(THIRTEENMILLION));
 			System.out.println("Tab: " + Tab);
 
 			// Unsure, seems to work; needs more testing
@@ -122,11 +125,11 @@ public class PiChudnovsky {
 			if (bi.equals(BigInteger.ONE)) {
 				System.out.println("bitshift");
 				System.out.println("Tab before bitshift: " + Tab);
-				Tab = Tab.multiply(new BigDecimal(MINUS_ONE));
+				Tab = Tab.multiply(MINUS_ONE);
 				System.out.println("Tab bitshift: " + Tab);
 			}
 		} else {
-			System.out.println("Inside else");
+			System.out.println("Inside else 2");
 
 			// Recursively compute P(a,b), Q(a,b) and T(a,b)
 			// m is the midpoint of a and b
@@ -134,16 +137,23 @@ public class PiChudnovsky {
 			System.out.println("M: " + m);
 
 			// Recursively calculate P(a,m), Q(a,m) and T(a,m)
-			LinkedList<BigDecimal> list_am = calculateTerms(a, m);
-			BigDecimal Pam = list_am.get(0);
-			BigDecimal Qam = list_am.get(1);
-			BigDecimal Tam = list_am.get(2);
+			LinkedList<BigInteger> list_am = calculateTerms(a, m);
+			BigInteger Pam = list_am.get(0);
+			BigInteger Qam = list_am.get(1);
+			BigInteger Tam = list_am.get(2);
 
 			// Recursively calculate P(m,b), Q(m,b) and T(m,b)
-			LinkedList<BigDecimal> list_mb = calculateTerms(m, b);
-			BigDecimal Pmb = list_mb.get(0);
-			BigDecimal Qmb = list_mb.get(1);
-			BigDecimal Tmb = list_mb.get(2);
+			LinkedList<BigInteger> list_mb = calculateTerms(m, b);
+			BigInteger Pmb = list_mb.get(0);
+			BigInteger Qmb = list_mb.get(1);
+			BigInteger Tmb = list_mb.get(2);
+
+			System.out.println("Pam in else 2: " + Pam);
+			System.out.println("Qam in else 2: " + Qam);
+			System.out.println("Tam in else 2: " + Tam);
+			System.out.println("Pmb in else 2: " + Pmb);
+			System.out.println("Qmb in else 2: " + Qmb);
+			System.out.println("Tmb in else 2: " + Tmb);
 
 			// Now combine
 			Pab = Pam.multiply(Pmb);
@@ -156,7 +166,7 @@ public class PiChudnovsky {
 		}
 
 		// Return Pab, Qab and Tab in a list
-		LinkedList<BigDecimal> list = new LinkedList<BigDecimal>();
+		LinkedList<BigInteger> list = new LinkedList<BigInteger>();
 		list.add(Pab);
 		list.add(Qab);
 		list.add(Tab);
