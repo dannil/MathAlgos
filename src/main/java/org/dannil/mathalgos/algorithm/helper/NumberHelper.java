6d80b65f1b37eff5fbf25c9d3f9403a1a287d47e
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-10-28 19:41:14 +0100

    Removed unused methods

--- src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
+++ src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
@@ -1,7 +1,6 @@
 package org.dannil.mathalgos.algorithm.helper;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -38,28 +37,6 @@ public class NumberHelper {
 	public static BigDecimal sqrt(BigDecimal value) {
 		BigDecimal x = new BigDecimal(Math.sqrt(value.doubleValue()));
 		return x.add(new BigDecimal(value.subtract(x.multiply(x)).doubleValue() / (x.doubleValue() * 2.0)));
-	}
-
-	private static final BigDecimal SQRT_DIG = new BigDecimal(150);
-	private static final BigDecimal SQRT_PRE = new BigDecimal(10).pow(SQRT_DIG.intValue());
-
-	private static BigDecimal sqrtNewtonRaphson(BigDecimal c, BigDecimal xn, BigDecimal precision) {
-		BigDecimal fx = xn.pow(2).add(c.negate());
-		BigDecimal fpx = xn.multiply(new BigDecimal(2));
-		BigDecimal xn1 = fx.divide(fpx, 2 * SQRT_DIG.intValue(), RoundingMode.HALF_DOWN);
-		xn1 = xn.add(xn1.negate());
-		// ----
-		BigDecimal currentSquare = xn1.pow(2);
-		BigDecimal currentPrecision = currentSquare.subtract(c);
-		currentPrecision = currentPrecision.abs();
-		if (currentPrecision.compareTo(precision) <= -1) {
-			return xn1;
-		}
-		return sqrtNewtonRaphson(c, xn1, precision);
-	}
-
-	public static BigDecimal bigSqrt(BigDecimal c) {
-		return sqrtNewtonRaphson(c, new BigDecimal(1), new BigDecimal(1).divide(SQRT_PRE));
 	}
 
 }
