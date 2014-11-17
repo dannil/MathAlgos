b3303e5703d15d086564a761730a4b4a9fd79164
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-17 22:33:16 +0100

    Added Nilakantha series for calculating pi

---
+++ src/main/java/org/dannil/mathalgos/algorithm/PiNilakantha.java
@@ -0,0 +1,40 @@
+package org.dannil.mathalgos.algorithm;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+
+public class PiNilakantha {
+
+	private static final RoundingMode roundingMode = RoundingMode.HALF_EVEN;
+
+	private static final BigInteger TWO = new BigInteger("2");
+	private static final BigInteger THREE = new BigInteger("3");
+	private static final BigInteger FOUR = new BigInteger("4");
+
+	private static BigInteger multiplications;
+	private static BigDecimal divisions;
+
+	public static final void debug() {
+		System.out.println(computePi(new BigInteger("1000")));
+	}
+
+	private static final BigDecimal computePi(final BigInteger iterations) {
+		BigDecimal pi = new BigDecimal("3.0");
+		BigInteger decimalPlaces = iterations.multiply(THREE);
+		boolean plus = true;
+
+		for (BigInteger k = THREE; k.compareTo(decimalPlaces) < 0; k = k.add(TWO)) {
+			multiplications = (k.subtract(BigInteger.ONE).multiply(k).multiply(k.add(BigInteger.ONE)));
+			divisions = new BigDecimal(FOUR).divide(new BigDecimal(multiplications), decimalPlaces.intValue(), roundingMode);
+			if (plus) {
+				pi = pi.add(divisions);
+			} else {
+				pi = pi.subtract(divisions);
+			}
+			plus = !plus;
+		}
+
+		return pi;
+	}
+}
