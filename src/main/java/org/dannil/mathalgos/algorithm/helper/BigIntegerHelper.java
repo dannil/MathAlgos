2dafc42d879cb2461ca988fe3e9dd12873dd4756
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-18 19:35:40 +0100

    Added modifiers and optimized code

--- src/main/java/org/dannil/mathalgos/algorithm/helper/BigIntegerHelper.java
+++ src/main/java/org/dannil/mathalgos/algorithm/helper/BigIntegerHelper.java
@@ -1,36 +1,12 @@
 package org.dannil.mathalgos.algorithm.helper;
 
 import java.math.BigInteger;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
-public class NumberHelper {
+public class BigIntegerHelper {
 
 	private static final BigInteger MINUS_ONE = new BigInteger("-1");
 	private static final BigInteger TWO = new BigInteger("2");
 	private static final BigInteger FOUR = new BigInteger("4");
-
-	/***
-	 * Splits a number into its digits.
-	 * 
-	 * @param t
-	 *            - The number to split
-	 *            
-	 * @return The digits of the splitted number
-	 */
-	public static List<Integer> splitIntoDigits(int t) {
-		// System.out.println("split");
-		List<Integer> temp = new LinkedList<Integer>();
-		int number = t;
-		while (number > 0) {
-			// System.out.println(number);
-			temp.add(number % 10);
-			number = number / 10;
-		}
-		Collections.reverse(temp);
-		return temp;
-	}
 
 	/**
 	 * Calculates the square root of the supplied BigInteger.
@@ -40,7 +16,7 @@ public class NumberHelper {
 	 * 
 	 * @return The square root of x
 	 */
-	public static BigInteger sqrt(BigInteger x) {
+	public static final BigInteger sqrt(BigInteger x) {
 		BigInteger div = BigInteger.ZERO.setBit(x.bitLength() / 2);
 		BigInteger div2 = div;
 		// Loop until we hit the same value twice in a row, or wind
@@ -54,13 +30,12 @@ public class NumberHelper {
 		}
 	}
 
-	public static BigInteger taylorSqrt(BigInteger x) {
-		final BigInteger MINUS_ONE = new BigInteger("-1");
-
+	public static final BigInteger taylorSqrt(BigInteger x) {
 		BigInteger result = new BigInteger("0");
 		BigInteger n = x.subtract(BigInteger.ONE);
 
 		for (BigInteger k = new BigInteger("1"); k.compareTo(n) < 0; k = k.add(BigInteger.ONE)) {
+			System.out.println("factorial: " + factorial(k));
 			System.out.println("loop");
 			BigInteger top = MINUS_ONE.pow(k.intValue()).multiply(factorial(k.multiply(TWO)));
 			BigInteger bottom_left = factorial(FOUR.pow(k.intValue()).multiply(k)).pow(2);
@@ -68,6 +43,7 @@ public class NumberHelper {
 			BigInteger bottom = bottom_left.multiply(bottom_right);
 			System.out.println("top: " + top);
 			System.out.println("bottom: " + bottom);
+			System.out.println("top/bottom: " + (top.divide(bottom)));
 			result = result.add(top.divide(bottom).multiply(n.pow(k.intValue())));
 		}
 		return result;
@@ -81,19 +57,18 @@ public class NumberHelper {
 	 * 
 	 * @return The logarithm for x
 	 */
-	public static BigInteger log10(final BigInteger x) {
+	public static final BigInteger log10(final BigInteger x) {
 		return new BigInteger(Integer.toString(x.toString().length() - 1));
 	}
 
-	public static BigInteger factorial(final BigInteger x) {
-		if (x.equals(BigInteger.ZERO))
-			return new BigInteger("1");
+	public static final BigInteger factorial(final BigInteger x) {
+		if (x.equals(BigInteger.ZERO)) {
+			return BigInteger.ONE;
+		}
 
-		BigInteger i = x.subtract(BigInteger.ONE);
 		BigInteger n = x;
-		while (i.compareTo(BigInteger.ZERO) > 0) {
+		for (BigInteger i = x.subtract(BigInteger.ONE); i.compareTo(BigInteger.ZERO) > 0; i = i.subtract(BigInteger.ONE)) {
 			n = n.multiply(i);
-			i = i.subtract(BigInteger.ONE);
 		}
 		return n;
 	}
