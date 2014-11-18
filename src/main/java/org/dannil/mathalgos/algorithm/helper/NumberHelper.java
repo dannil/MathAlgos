f8e3c33f22b0f1d2fd0634c3400ac93cb29c9619
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-18 19:19:46 +0100

    Added taylor sqrt

--- src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
+++ src/main/java/org/dannil/mathalgos/algorithm/helper/NumberHelper.java
@@ -7,6 +7,10 @@ import java.util.List;
 
 public class NumberHelper {
 
+	private static final BigInteger MINUS_ONE = new BigInteger("-1");
+	private static final BigInteger TWO = new BigInteger("2");
+	private static final BigInteger FOUR = new BigInteger("4");
+
 	/***
 	 * Splits a number into its digits.
 	 * 
@@ -50,6 +54,25 @@ public class NumberHelper {
 		}
 	}
 
+	public static BigInteger taylorSqrt(BigInteger x) {
+		final BigInteger MINUS_ONE = new BigInteger("-1");
+
+		BigInteger result = new BigInteger("0");
+		BigInteger n = x.subtract(BigInteger.ONE);
+
+		for (BigInteger k = new BigInteger("1"); k.compareTo(n) < 0; k = k.add(BigInteger.ONE)) {
+			System.out.println("loop");
+			BigInteger top = MINUS_ONE.pow(k.intValue()).multiply(factorial(k.multiply(TWO)));
+			BigInteger bottom_left = factorial(FOUR.pow(k.intValue()).multiply(k)).pow(2);
+			BigInteger bottom_right = BigInteger.ONE.subtract(k.multiply(TWO));
+			BigInteger bottom = bottom_left.multiply(bottom_right);
+			System.out.println("top: " + top);
+			System.out.println("bottom: " + bottom);
+			result = result.add(top.divide(bottom).multiply(n.pow(k.intValue())));
+		}
+		return result;
+	}
+
 	/**
 	 * Calculates the 10 logarithm for the supplied BigInteger.
 	 * 
@@ -60,6 +83,19 @@ public class NumberHelper {
 	 */
 	public static BigInteger log10(final BigInteger x) {
 		return new BigInteger(Integer.toString(x.toString().length() - 1));
+	}
+
+	public static BigInteger factorial(final BigInteger x) {
+		if (x.equals(BigInteger.ZERO))
+			return new BigInteger("1");
+
+		BigInteger i = x.subtract(BigInteger.ONE);
+		BigInteger n = x;
+		while (i.compareTo(BigInteger.ZERO) > 0) {
+			n = n.multiply(i);
+			i = i.subtract(BigInteger.ONE);
+		}
+		return n;
 	}
 
 }
