450142b9cb9a080885e9cf514645ea53c0595b1a
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2014-11-19 20:39:14 +0100

    Added modifiers

--- src/main/java/org/dannil/mathalgos/math/BigDecimalMath.java
+++ src/main/java/org/dannil/mathalgos/math/BigDecimalMath.java
@@ -4,11 +4,11 @@ import java.math.BigDecimal;
 
 public class BigDecimalMath {
 
-	private static BigDecimal TWO = new BigDecimal(2);
-	private static BigDecimal TEN = new BigDecimal(10);
-	private static BigDecimal LOGTEN = new BigDecimal(Math.log(10));
-	private static BigDecimal LNTWO = new BigDecimal(Math.log(2));
-	private static BigDecimal MAXDOUBLE = new BigDecimal(Double.MAX_VALUE);
+	private static final BigDecimal TWO = new BigDecimal(2);
+	private static final BigDecimal TEN = new BigDecimal(10);
+	private static final BigDecimal LOGTEN = new BigDecimal(Math.log(10));
+	private static final BigDecimal LNTWO = new BigDecimal(Math.log(2));
+	private static final BigDecimal MAXDOUBLE = new BigDecimal(Double.MAX_VALUE);
 
 	public static BigDecimal log10(BigDecimal v) {
 		if (v.compareTo(MAXDOUBLE) > 0) {
