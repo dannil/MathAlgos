77913ab89c02450d66e1f62132c962f07cef1314
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-02-03 11:07:43 +0100

    Moved benchmark utility to test portion of project

--- src/main/java/org/dannil/mathalgos/debug/DebugPi.java
+++ src/main/java/org/dannil/mathalgos/debug/DebugPi.java
@@ -3,13 +3,13 @@ package org.dannil.mathalgos.debug;
 import java.math.BigInteger;
 
 import org.dannil.mathalgos.algorithm.PiChudnovsky;
-import org.dannil.mathalgos.algorithm.helper.BenchmarkHelper;
+import org.dannil.mathalgos.test.utility.BenchmarkUtility;
 
 public class DebugPi {
 	public static void main(String[] args) {
 		BigInteger big = new BigInteger("10000");
 
-		BenchmarkHelper bench = new BenchmarkHelper();
+		BenchmarkUtility bench = new BenchmarkUtility();
 
 		bench.startBench();
 		System.out.println(PiChudnovsky.computePi(big));
