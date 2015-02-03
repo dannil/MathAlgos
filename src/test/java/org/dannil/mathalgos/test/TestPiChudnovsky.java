eabb21625d0fb2c450ac6a6221f951c1bf487bbd
Daniel Nilsson <bobkant-fyrsvamp@hotmail.com>
2015-02-03 15:36:54 +0100

    Converted to parameterized test

--- src/test/java/org/dannil/mathalgos/test/TestPiChudnovsky.java
+++ src/test/java/org/dannil/mathalgos/test/TestPiChudnovsky.java
@@ -1,77 +1,43 @@
 package org.dannil.mathalgos.test;
 
 import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.log4j.Logger;
 import org.dannil.mathalgos.algorithm.PiChudnovsky;
-import org.dannil.mathalgos.test.utility.BenchmarkUtility;
 import org.dannil.mathalgos.test.utility.FileUtility;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public final class TestPiChudnovsky {
 
 	private final static Logger LOGGER = Logger.getLogger(TestPiChudnovsky.class.getName());
 
-	private BenchmarkUtility benchmarkUtility;
+	private String expected;
+	private String actual;
+
+	public TestPiChudnovsky(final String expected, final String actual) {
+		this.expected = expected;
+		this.actual = actual;
+	}
+
+	@Parameters
+	public static Collection<String[]> generatedPi() {
+		return Arrays.asList(new String[][] { { FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=1000]/value"), PiChudnovsky.computePiStringPresentation(new BigInteger("1000")) },
+				{ FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=10000]/value"), PiChudnovsky.computePiStringPresentation(new BigInteger("10000")) },
+				{ FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=100000]/value"), PiChudnovsky.computePiStringPresentation(new BigInteger("100000")) } });
+	}
 
 	// ------ PI ------ //
 
-	@Before
-	public final void initialize() {
-		this.benchmarkUtility = new BenchmarkUtility();
-	}
-
 	@Test
-	public final void chudnovskyTo1000Places() {
-		final String length = "1000";
-
-		String s = FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=" + length + "]/value");
-
-		this.benchmarkUtility.startBench();
-		Assert.assertEquals(s, PiChudnovsky.computePiStringPresentation(new BigInteger(length)));
-		this.benchmarkUtility.stopBench();
-
-		LOGGER.info(this.benchmarkUtility.toString());
+	public final void calculatePiWithChudnovsky() {
+		Assert.assertEquals(this.expected, this.actual);
 	}
 
-	@Test
-	public final void chudnovskyTo10000Places() {
-		final String length = "10000";
-
-		String s = FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=" + length + "]/value");
-
-		this.benchmarkUtility.startBench();
-		Assert.assertEquals(s, PiChudnovsky.computePiStringPresentation(new BigInteger(length)));
-		this.benchmarkUtility.stopBench();
-
-		LOGGER.info(this.benchmarkUtility.toString());
-	}
-
-	@Test
-	public final void chudnovskyTo100000Places() {
-		final String length = "100000";
-
-		String s = FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=" + length + "]/value");
-
-		this.benchmarkUtility.startBench();
-		Assert.assertEquals(s, PiChudnovsky.computePiStringPresentation(new BigInteger(length)));
-		this.benchmarkUtility.stopBench();
-
-		LOGGER.info(this.benchmarkUtility.toString());
-	}
-
-	// @Test
-	public final void chudnovskyTo1000000Places() {
-		final String length = "1000000";
-
-		String s = FileUtility.getPiXmlValue("pi.xml", "/pilist/pi[@length=" + length + "]/value");
-
-		this.benchmarkUtility.startBench();
-		Assert.assertEquals(s, PiChudnovsky.computePiStringPresentation(new BigInteger(length)));
-		this.benchmarkUtility.stopBench();
-
-		LOGGER.info(this.benchmarkUtility.toString());
-	}
 }
